c3dbd1e844c51624a046b3eac88091936875df6c
dwiki <dwikis17@gmail.com>
2026-07-27 20:29:47 +0700

    initial

---
+++ shengci/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  shengci
+//
+//  Created by Dwiki on 27/07/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ shengciTests/shengciTests.swift
@@ -0,0 +1,19 @@
+//
+//  shengciTests.swift
+//  shengciTests
+//
+//  Created by Dwiki on 27/07/26.
+//
+
+import Testing
+@testable import shengci
+
+struct shengciTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
